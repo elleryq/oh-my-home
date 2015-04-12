6af306b32a0877969c2c71b2cee9cfb16739fce9
Yan-ren Tsai <elleryq@gmail.com>
2015-04-12 22:42:28 +0800

    Update installer script.

--- bin/upgrade_calibre.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main()"
-

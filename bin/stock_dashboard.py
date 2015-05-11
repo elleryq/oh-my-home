c7582ca2f99187cd327f36e7de6fe3aca217985c
elleryq <elleryq@gmail.com>
2015-05-11 09:56:10 +0800

    Move stocks to .config/stock_dashboard.ini

--- bin/stock_dashboard.py
+++ bin/stock_dashboard.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 from __future__ import print_function
+import os
 from grs import RealtimeTWSE
+
+
+def getStocksFromConfig():
+    from ConfigParser import SafeConfigParser
+
+    configFile = os.path.join(
+        os.path.expanduser("~/.config"),
+        "stock_dashboard.ini")
+    if not os.path.exists(configFile):
+        return ['0050', '0051']
+
+    parser = SafeConfigParser()
+    parser.read(configFile)
+    if not parser.has_option('main', 'stocks'):
+        return ['0050', '0051']
+
+    stocks = parser.get('main', 'stocks')
+    return stocks.split(',')
 
 def getRealtimeStock(stock_no):
     try:
@@ -13,7 +32,8 @@ def getRealtimeStock(stock_no):
 
 
 def main():
-    stocks = ['0050', '0051', '2347']
+    #stocks = ['0050', '0051', '2347']
+    stocks = getStocksFromConfig()
     results = [getRealtimeStock(stock_no) for stock_no in stocks]
     for result in results:
         if not result:

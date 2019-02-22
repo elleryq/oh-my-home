9eaf2c14243ef5d3ef3deb7b34c03e77b40eb8e9
elleryq <elleryq@gmail.com>
2019-02-22 10:12:09 +0800

    Use xargs -r to prevent no 'rc' packages

--- bin/purge_unused_packages.sh
+++ bin/purge_unused_packages.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo dpkg --purge `dpkg -l | awk '/^rc/{print $2;}' `
+# sudo dpkg --purge `dpkg -l | awk '/^rc/{print $2;}' `
+dpkg -l | awk '/^rc/{print $2;}' | xargs -r sudo dpkg --purge
